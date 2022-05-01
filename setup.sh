c299ce68024fc96d819b41115d3ac31c08a828ab
Ueno Takahiro <uenotakahiro.jp@gmail.com>
2022-05-01 18:27:06 +0900

    Fix bug

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ sudo chsh -s /bin/zsh "$USER"
 
 # Install zprezto
 git clone --recursive https://github.com/utaka/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-git switch utaka
+(cd "${ZDOTDIR:-$HOME}/.zprezto" && git switch utaka)
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
