e13dfecd05eed562be55dc6e1ba137f9aab9f4bd
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2022-06-10 13:24:15 +0900

    Fix path of gibo

--- setup.sh
+++ setup.sh
@@ -41,7 +41,7 @@ nvm install node'
 
 # Install gibo
 curl -L https://raw.github.com/simonwhitaker/gibo/master/gibo \
-    -so ~/bin/gibo && chmod +x ~/bin/gibo && gibo update
+    -so "$HOME/bin/gibo" && chmod +x "$HOME/bin/gibo" && "$HOME/bin/gibo" update
 
 ### Config
 # Configure Git environment
