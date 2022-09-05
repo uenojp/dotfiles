341afe123f838421c8c8998b3a157b1ecd30ed92
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2022-09-05 16:40:08 +0900

    Update

--- install/docker.sh
+++ install/docker.sh
@@ -14,15 +14,21 @@ sudo apt-get install -y \
     lsb-release \
     ;
 
-[ ! -e /etc/apt/keyrings/docker.gpg ] \
+[ ! -e /usr/share/keyrings/docker.gpg ] \
     && curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
-    | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
 [ ! -e /etc/apt/sources.list.d/docker.list ] \
-    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
     | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo usermod -aG docker "$USER"
 
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y \
+    containerd.io \
+    docker-ce \
+    docker-ce-cli \
+    docker-compose-plugin \
+    ;
+
+sudo usermod -aG docker "$USER"
 
