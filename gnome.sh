19335e39c2ab619f81fb3d0c9d813d50fc373446
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2024-01-18 03:36:49 +0900

    Remove prezto

--- gnome.sh
+++ gnome.sh
@@ -5,16 +5,13 @@ set -eux
 # Swap caps and ctrl
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:swapcaps']"
-
 # Set input sources to English and Japanese
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'mozc-jp')]"
 # Switch between English and Japanese with Ctrl+Space
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Primary>space']"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Primary><Shift>space']"
-
 # Hide Dock
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
-
 # Change UI font
 gsettings set org.gnome.desktop.interface font-name "Noto Sans CJK JP 11"
 
--- setup.sh
+++ setup.sh
@@ -4,73 +4,45 @@ set -eux
 
 mkdir -p "$HOME/bin"
 
-#
-# Install
-#
 sudo apt-get update
 sudo apt-get upgrade -y
 
 sudo apt-get install -y \
+    arp-scan \
     bat \
     build-essential \
     clang-format \
     cmake \
     curl \
+    foot \
     gawk \
     gdb \
     gh \
-    gnome-shell-pomodoro \
-    jq \
-    nkf \
-    rename \
     shellcheck \
     tmux \
     tree \
     vim-gtk \
-    w3m \
     wget \
+    wireshark \
     xclip \
     zsh \
     ;
 chsh -s /bin/zsh "$USER"
 
-./install/zprezto.sh
-./install/nvm.sh
-./install/gibo.sh
-./install/google-chrome.sh
-
-#
-# Config
-#
-# Configure Git environment
-git config --global user.name "Takahiro Ueno"
-git config --global user.email "uenotakahiro.jp@gmail.com"
-git config --global color.ui auto
-git config --global core.editor vim
-git config --global init.defaultBranch main
-
-git config --global alias.remote-ssh $'!git remote set-url origin "$(git remote -v | head -1 | awk \'{print $2}\' | sed \'s_https://_git@_; s_/_:_\')"'
-git config --global alias.prev 'checkout HEAD^'
-git config --global alias.next $'!git checkout "$(git log --format=%H "$(git name-rev --name-only HEAD | sed \'s/~.*$//\')" | grep -B1 -m1 "$(git rev-parse HEAD)" | head -1)"'
-
-# Create symlink
 backup() {
     targets=("$@")
-    for target in ${targets[@]};do
+    for target in "${targets[@]}";do
         [ -e "$target" ] && mv "$target" "${target}.bak"
     done
-    return 0
 }
 
-backup "$HOME/.clang-format"       && ln -s "$HOME/.dotfiles/clang-format" "$HOME/.clang-format"
-backup "$HOME/.latexmkrc"          && ln -s "$HOME/.dotfiles/latexmkrc" "$HOME/.latexmkrc"
-backup "$HOME/.tmux.conf"          && ln -s "$HOME/.dotfiles/tmux.conf" "$HOME/.tmux.conf"
-backup "$HOME/.vim" "$HOME/.vimrc" && ln -s "$HOME/.dotfiles/vim" "$HOME/.vim"
+backup "$HOME/.clang-format"       && ln -s "$HOME/.dotfiles/clang-format"  "$HOME/.clang-format"
+backup "$HOME/.latexmkrc"          && ln -s "$HOME/.dotfiles/latexmkrc"     "$HOME/.latexmkrc"
+backup "$HOME/.tmux.conf"          && ln -s "$HOME/.dotfiles/tmux.conf"     "$HOME/.tmux.conf"
+backup "$HOME/.vim" "$HOME/.vimrc" && ln -s "$HOME/.dotfiles/vim"           "$HOME/.vim"
 
 ln -s "/usr/bin/batcat" "$HOME/bin/bat"
-ln -s "$HOME/.dotfiles/bin/dict" "$HOME/bin/dict"
-ln -s "$HOME/.dotfiles/bin/line" "$HOME/bin/line"
-ln -s "$HOME/.dotfiles/bin/mjoin" "$HOME/bin/mjoin"
-ln -s "$HOME/.dotfiles/bin/obs" "$HOME/bin/obs"
-ln -s "$HOME/.dotfiles/bin/o" "$HOME/bin/o"
+ln -s "$HOME/.dotfiles/bin/line"    "$HOME/bin/line"
+ln -s "$HOME/.dotfiles/bin/obs"     "$HOME/bin/obs"
+ln -s "$HOME/.dotfiles/bin/o"       "$HOME/bin/o"
 
