b2c2bccb5d16bb7de80110d07f15eb60d4fa8ec6
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2024-07-06 06:19:15 +0900

    Activate Hiragana input mode by default in Mozc

--- gnome.sh
+++ gnome.sh
@@ -7,6 +7,10 @@ gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:swapcaps']"
 # Set input sources to English and Japanese
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'mozc-jp')]"
+# Activate Hiragana input instead of Direct input in default mode
+# see https://github.com/google/mozc/blob/master/docs/configurations.md#activate-mozc-on-launch
+[ -r "$HOME/.config/mozc/ibus_config.textproto" ] &&
+    sed -i '/active_on_launch/s@False@True@' "$HOME/.config/mozc/ibus_config.textproto" # assuming GNU sed (-i option)
 # Switch between English and Japanese with Ctrl+Space
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Primary>space']"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Primary><Shift>space']"
