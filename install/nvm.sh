294a69515b2b78bfcea0afdf8f5a59082e7e9287
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2024-08-27 17:54:17 +0900

    Update install/nvm.sh

--- install/nvm.sh
+++ install/nvm.sh
@@ -6,6 +6,6 @@
 NVM_LATEST=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | awk '/tag_name/{print $2}' | tr -d '",')
 SHELL=/bin/zsh
 curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_LATEST/install.sh" | bash
-source "$HOME/.zshrc"
+source "${ZDOTDIR-HOME}/.zshenv"
 nvm install node
 
