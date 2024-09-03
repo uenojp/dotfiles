41559a09f0550133b4c2678cfdeb876095b3645f
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2024-09-03 22:29:26 +0900

    Disable wezterm title bar

--- wezterm.lua
+++ wezterm.lua
@@ -5,6 +5,7 @@ return {
   use_ime = true,
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
+  window_decorations = "RESIZE",
 
   -- Keybindgs: similar to tmux keybindings
   leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 },
