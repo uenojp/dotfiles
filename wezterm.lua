9f4fa4c3be3316d5060931a8ddd6e3b8659588bc
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2024-12-19 15:10:11 +0900

    Set font size to 16 in WezTerm conf

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 
 return {
   font = wezterm.font_with_fallback({ "Cica", "JetBrains Mono" }),
+  font_size = 16.0,
   use_ime = true,
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
